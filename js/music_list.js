fc227e4f2e00ea5375234b150f543392c0c3ca77
jiyeon6912 <jiyeon6912@gmail.com>
2020-11-26 17:44:16 +0900

    ver9

--- js/music_list.js
+++ js/music_list.js
@@ -4,7 +4,7 @@ const musicList = [
     director: '신카이 마코토',
     dataitem: 'ani',
     music: 'Rain - 秦基博(Motohiro Hata)',
-    poster: './images/home/nail-1.jpg',
+    poster: './images/music/garden.jpg',
     youtube: 'http://www.youtube.com/watch?v=eW0Glxi30C0',
     text:
       '영화는 구두장이를 꿈꾸는 15세의 아키즈키 타카오와 27세의 여인 유키노 유카리가 비 오는 아침마다 신주쿠 공원에서 이어가는 만남을 그린다. 타카오는 구두 디자인을 하기 위해 등교를 늦게 하고 유키노는 직장에서의 개인적 문제 때문에 일을 나가지 않는다.'
@@ -36,42 +36,42 @@ const musicList = [
     title: '날씨의아이',
     director: '신카이 마코토',
     dataitem: 'ani',
-    music: 'Rain - 秦基博(Motohiro Hata)',
-    poster: './images/home/nail-1.jpg',
+    music: '사랑이 할 수 있는 일이 아직 있을까',
+    poster: './images/music/weather.jpg',
     youtube: 'http://www.youtube.com/watch?v=eNMiwMuw-yo',
     text:
       '영화는 구두장이를 꿈꾸는 15세의 아키즈키 타카오와 27세의 여인 유키노 유카리가 비 오는 아침마다 신주쿠 공원에서 이어가는 만남을 그린다. 타카오는 구두 디자인을 하기 위해 등교를 늦게 하고 유키노는 직장에서의 개인적 문제 때문에 일을 나가지 않는다.'
   },
 {
     id: 5,
-    title: '별을 쫓는 아이',
+    title: '초속 5cm',
     director: '신카이 마코토',
     dataitem: 'ani',
-    music: 'Hello Goodbye & Hello',
-    poster: './images/home/nail-2.jpg',
-    youtube: 'http://www.youtube.com/watch?v=UTJbCQATrgI',
+    music: ' One More TIme, One More Chance',
+    poster: './images/music/5cm.jpg',
+    youtube: 'http://www.youtube.com/watch?v=a1Qc5pnurp4',
     text:
       '주인공은 10대 초반의 어린 소녀 아스나의 눈을 통해 모험  액션  사랑을 그리고 있다.'
   },
 {
     id: 6,
-    title: '분노의 질주:홉스앤쇼',
-    director: '데이비드 리치',
-    dataitem: 'movie',
-    music: 'Time in a bottle - YUNGBLUD ',
-    poster: './images/home/nail-3.jpg',
-    youtube: 'http://www.youtube.com/watch?v=JVVpuZq9jlc',
+    title: '너의 이름은',
+    director: '신카이 마코토',
+    dataitem: 'ani',
+    music: 'Sparkle',
+    poster: './images/music/name.jpg',
+    youtube: 'http://www.youtube.com/watch?v=CYTnZV_NywU',
     text:
       '분노의 질주 시리즈의 첫 스핀오프 영화이며  데커드 쇼와 루크 홉스의 팀업 영화이다.'
   },
   {
     id: 7,
-    title: '언어의정원',
-    director: '신카이 마코토',
-    dataitem: 'ani',
-    music: 'Rain - 秦基博(Motohiro Hata)',
-    poster: './images/home/nail-1.jpg',
-    youtube: 'http://www.youtube.com/watch?v=eW0Glxi30C0',
+    title: '뮬란',
+    director: '니키 카로',
+    dataitem: 'movie',
+    music: 'Christina Aguilera - Reflection',
+    poster: './images/music/mulan.jpg',
+    youtube: 'http://www.youtube.com/watch?v=o_S-P0AK9ec',
     text:
       '영화는 구두장이를 꿈꾸는 15세의 아키즈키 타카오와 27세의 여인 유키노 유카리가 비 오는 아침마다 신주쿠 공원에서 이어가는 만남을 그린다. 타카오는 구두 디자인을 하기 위해 등교를 늦게 하고 유키노는 직장에서의 개인적 문제 때문에 일을 나가지 않는다.'
   },
