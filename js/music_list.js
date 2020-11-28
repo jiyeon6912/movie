00eedfd337888a1ba1ad145d2b6cddb2b05921c3
unknown <jiyeon6912@gmail.com>
2020-11-28 21:01:40 +0900

    ver9

--- js/music_list.js
+++ js/music_list.js
@@ -3,7 +3,7 @@ const musicList = [
     title: '언어의정원',
     director: '신카이 마코토',
     dataitem: 'ani',
-    music: 'Rain - 秦基博(Motohiro Hata)',
+    music: 'Younha - Umbrella M/V',
     poster: './images/music/garden.jpg',
     youtube: 'http://www.youtube.com/watch?v=eW0Glxi30C0',
     text:
@@ -27,7 +27,7 @@ const musicList = [
     dataitem: 'movie',
     music: 'Time in a bottle - YUNGBLUD ',
     poster: './images/home/nail-3.jpg',
-    youtube: 'http://www.youtube.com/watch?v=JVVpuZq9jlc',
+    youtube: 'http://www.youtube.com/watch?v=iQxo3FNvagE',
     text:
       '분노의 질주 시리즈의 첫 스핀오프 영화이며  데커드 쇼와 루크 홉스의 팀업 영화이다.'
   },
@@ -51,7 +51,7 @@ const musicList = [
     poster: './images/music/5cm.jpg',
     youtube: 'http://www.youtube.com/watch?v=a1Qc5pnurp4',
     text:
-      '주인공은 10대 초반의 어린 소녀 아스나의 눈을 통해 모험  액션  사랑을 그리고 있다.'
+      '주인공은 10대 초반의 어린 소녀 아스나의 눈을 통해 모험 액션 사랑을 그리고 있다.'
   },
 {
     id: 6,
@@ -69,20 +69,20 @@ const musicList = [
     title: '뮬란',
     director: '니키 카로',
     dataitem: 'movie',
-    music: 'Christina Aguilera - Reflection',
+    music: 'Loyal Brave True',
     poster: './images/music/mulan.jpg',
-    youtube: 'http://www.youtube.com/watch?v=o_S-P0AK9ec',
+    youtube: 'http://www.youtube.com/watch?v=vJI-2LwRCGQ',
     text:
-      '영화는 구두장이를 꿈꾸는 15세의 아키즈키 타카오와 27세의 여인 유키노 유카리가 비 오는 아침마다 신주쿠 공원에서 이어가는 만남을 그린다. 타카오는 구두 디자인을 하기 위해 등교를 늦게 하고 유키노는 직장에서의 개인적 문제 때문에 일을 나가지 않는다.'
+      '작품설명'
   },
 {
     id: 8,
-    title: '별을 쫓는 아이',
-    director: '신카이 마코토',
-    dataitem: 'ani',
-    music: 'Hello Goodbye & Hello',
-    poster: './images/home/nail-2.jpg',
-    youtube: 'http://www.youtube.com/watch?v=UTJbCQATrgI',
+    title: '아이언맨3',
+    director: '셰인 블랙',
+    dataitem: 'movie',
+    music: 'Blue(Da Ba Dee)',
+    poster: './images/music/iron.jpg',
+    youtube: 'ttp://www.youtube.com/watch?v=qfrVquUyUN8',
     text:
       '주인공은 10대 초반의 어린 소녀 아스나의 눈을 통해 모험  액션  사랑을 그리고 있다.'
   },
@@ -92,10 +92,42 @@ const musicList = [
     director: '왕가위',
     dataitem: 'movie',
     music: 'California Dreamin',
-    poster: './images/home/nail-3.jpg',
+    poster: './images/music/gc.jpg',
     youtube: 'http://www.youtube.com/watch?v=dV-N5ZOzblY',
     text:
-      '분노의 질주 시리즈의 첫 스핀오프 영화이며  데커드 쇼와 루크 홉스의 팀업 영화이다.'
+      '작품설명'
+  },
+  { id: 10,
+    title: '언어의정원',
+    director: '신카이 마코토',
+    dataitem: 'ani',
+    music: 'Rain - 秦基博(Motohiro Hata)',
+    poster: './images/music/garden_2.jpg',
+    youtube: 'http://www.youtube.com/watch?v=8uO2X2Rc4LU',
+    text:
+      '영화는 구두장이를 꿈꾸는 15세의 아키즈키 타카오와 27세의 여인 유키노 유카리가 비 오는 아침마다 신주쿠 공원에서 이어가는 만남을 그린다. 타카오는 구두 디자인을 하기 위해 등교를 늦게 하고 유키노는 직장에서의 개인적 문제 때문에 일을 나가지 않는다.'
+  },
+  {
+    id: 11,
+    title: '엑스맨3',
+    director: '브렛 래트너',
+    dataitem: 'movie',
+    music: 'Sweet dreams',
+    poster: './images/music/xman3.jpg',
+    youtube: 'http://www.youtube.com/watch?v=mxFIxEJN5KA',
+    text:
+      '작품설명'
+  },
+  {
+    id: 12,
+    title: 'Doctor Strange',
+    director: '브렛 래트너',
+    dataitem: 'movie',
+    music: 'Believer',
+    poster: './images/music/doctor.jpg',
+    youtube: 'http://www.youtube.com/watch?v=q2k7qdD4qtc',
+    text:
+      '작품설명'
   }
 ]
 
